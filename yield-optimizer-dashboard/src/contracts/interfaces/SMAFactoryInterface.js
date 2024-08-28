25ab685d537a5d50c8c4e7cddd96340bf2e7e00b
j1m5s3 <james.lynch.uml@gmail.com>
2024-08-27 23:58:38 -0500

    WIP...

--- yield-optimizer-dashboard/src/contracts/interfaces/SMAFactoryInterface.js
+++ yield-optimizer-dashboard/src/contracts/interfaces/SMAFactoryInterface.js
@@ -1,12 +1,25 @@
+// Local
+import { SMAFactoryABI } from "../abi/SMAFactoryABI";
+
+// External
 import { readContract, writeContract } from "@wagmi/core";
 
 class SMAFactoryInterface {
+    /*
+    * SMAFactoryInterface
+    * 
+    * Interface for the SMAFactory contract
+    * 
+    */
+   
     abi; // List of functions in the contract
     address; // Address of the contract
     config; // Configuration object (WagmiConfig)
 
-    constructor(config) {
+    constructor(config, network) {
         this.config = config;
+        this.abi = SMAFactoryABI.abi;
+        this.address = SMAFactoryABI.addresses[network];
     }
 
     readSMACount = async () => {
